0c746ce608124edd687cccc7a62b9efb5dd3aa05
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-16 22:46:29 -0300

    test: ensures signup return 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest'
+import { SignupController } from './signup'
+
+describe('Signup Controller', () => {
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
