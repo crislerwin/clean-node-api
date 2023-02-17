8e7371f0e55b97be6c462e06a14d572dc8de0be1
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-16 21:54:00 -0300

    chore: added prettier eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
-  }
+    indent: 'error',
+  },
 }
