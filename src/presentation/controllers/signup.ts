b4935349e56fd1522e41e3631647da592a3b49cd
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-22 10:13:31 -0300

    chore: returns 400 if email is not provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,5 +6,11 @@ export class SignupController {
         body: new Error('Missing param: name'),
       }
     }
+    if (httpRequest.body.email === undefined) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      }
+    }
   }
 }
