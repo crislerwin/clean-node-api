2ff7f76f4a4b7d613a9b947ea77769a32b7d4a6a
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-24 22:36:59 -0300

    chore: add test staged

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    env: {
+      NODE_ENV: 'test',
+    },
     coverage: { src: ['src/**/*.ts'], include: ['.ts'] },
   },
 })
