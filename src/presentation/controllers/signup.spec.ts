3bff97f365505cf4b830e754253952a1f900911d
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-24 23:43:08 -0300

    refactor: makeSut test factory

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,9 +2,12 @@ import { describe, expect, test } from 'vitest'
 import { MissingPararmError } from '../errors/missing-param-error'
 import { SignupController } from './signup'
 
+const makeSut = (): SignupController => {
+  return new SignupController()
+}
 describe('Signup Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -30,7 +33,7 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingPararmError('email'))
   })
   test('should return 400 if no password is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -42,7 +45,7 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingPararmError('password'))
   })
   test('should return 400 if no password confirmation is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
