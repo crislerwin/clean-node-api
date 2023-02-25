03f88bb06bcaed5fa422909a6241514b852ba0e7
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-24 23:40:18 -0300

    fix: coverage tests showing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    env: {
-      NODE_ENV: 'test',
-    },
-    coverage: { src: ['src/**/*.ts'], include: ['.ts'] },
-  },
+  test: {},
 })
