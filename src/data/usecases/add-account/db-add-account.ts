542b68f7301fc3787d38ca3e675d05e27a180705
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 22:30:03 -0300

    feat: ensure DbAccount calls Encrypter with correct password

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,17 @@
+import { Encrypter } from '@/data/protocols/encrypter'
+import { AccountModel } from '@/domain/models/account'
+import { AddAccount, AddAccountModel } from '@/domain/usecases/add-account'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise((resolve) => {
+      resolve(null as any)
+    })
+  }
+}
