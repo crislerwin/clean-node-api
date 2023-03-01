2780f345f6990c079d654023849e8270341dbf00
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 21:59:04 -0300

    test: ensure EmailValidatorAdapter returns ttrue if valid email is provided

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,17 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
+    vi.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+  test('Should return true if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
