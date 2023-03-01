05e988b66fb3bdb1ab6b31816b11689009ff5893
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 21:45:06 -0300

    spec: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest'
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
