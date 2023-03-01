279398b83a35060dd2835c930756407695fa5acf
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 22:03:49 -0300

    refactor: move sut create for a helper factory method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -2,21 +2,23 @@ import { describe, expect, test, vi } from 'vitest'
 import { EmailValidatorAdapter } from './email-validator'
 import validator from 'validator'
 
+const makeSut = (): EmailValidatorAdapter => new EmailValidatorAdapter()
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     vi.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
   test('Should return false if validator returns false', () => {
     const emailSpy = vi.spyOn(validator, 'isEmail')
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     sut.isValid('valid_email@mail.com')
     expect(emailSpy).toHaveBeenCalledWith('valid_email@mail.com')
   })
