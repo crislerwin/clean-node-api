c865916d7bbb5af3a40d8f3768f1716358c3dd59
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 22:58:33 -0300

    refactor: move all protocols to a single file

---
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/domain/usecases/add-account'
+export * from '@/data/protocols/encrypter'
+export * from '@/domain/models/account'
--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from '@/data/protocols/encrypter'
+import { Encrypter } from './db-add-account-protocols'
 import { describe, expect, test, vi } from 'vitest'
 import { DbAddAccount } from './db-add-account'
 
