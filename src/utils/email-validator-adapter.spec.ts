f428a84d607e50f1102158737f3bbf746873a1f0
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-02-28 22:02:29 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -9,9 +9,15 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
-  test('Should return true if validator returns false', () => {
+  test('Should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
+  })
+  test('Should return false if validator returns false', () => {
+    const emailSpy = vi.spyOn(validator, 'isEmail')
+    const sut = new EmailValidatorAdapter()
+    sut.isValid('valid_email@mail.com')
+    expect(emailSpy).toHaveBeenCalledWith('valid_email@mail.com')
   })
 })
