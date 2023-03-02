49f1534a1e3130037104adf264517fd4c44d1316
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-02 08:33:57 -0300

    feat: ensure DbAddAccount returns account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,11 +11,9 @@ export class DbAddAccount implements AddAccount {
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    const newAccount = await this.addAccountRepository.add(
+    const account = await this.addAccountRepository.add(
       Object.assign(accountData, { password: hashedPassword }),
     )
-    return await new Promise((resolve) => {
-      resolve(newAccount)
-    })
+    return account
   }
 }
