88248f0afa8d48e594bc39d2b4f56b7bdd683a82
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-02 09:13:22 -0300

    refactor: move sut creator to a factory method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,17 +2,20 @@ import { describe, expect, test, vi } from 'vitest'
 import { BcryptAdapter } from './encrypt-adapter'
 import bcrypt from 'bcrypt'
 
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
 describe('BcryptAdapter', () => {
   test('Should call EncryptAdapter with correct values', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = vi.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toBeCalledWith('any_value', salt)
   })
   test('Should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSut()
     vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async (): Promise<string> => {
       return await new Promise((resolve) => {
         resolve('hash')
