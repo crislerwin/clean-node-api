b0df4af6fe69308925ce239e03e2bcf7131f41d3
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-05 16:47:18 -0300

    refactor: mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,21 +1,21 @@
-import { MongoClient, Collection, Document, WithId, ObjectId } from 'mongodb'
+import { MongoClient, Collection, Document, WithId } from 'mongodb'
 
-type MappedCollection = Record<string, any> & {
-  id: ObjectId
-}
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  uri: null as unknown as string,
   async connect(uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
   async disconnect(): Promise<void> {
     await this.client.close()
   },
-  async getCollection(name: string): Promise<Collection<Document>> {
+
+  getCollection(name: string): Collection {
     const collection = this.client.db().collection(name)
     return collection
   },
-  map(collection: WithId<Document> | null): MappedCollection | null {
+  map(collection: WithId<Document> | null): any {
     if (collection) {
       const { _id, ...collectionWithoutId } = collection
       return Object.assign({}, collectionWithoutId, { id: _id })
