b8ca5960b306a65b025119ce87ef8635ba825423
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-04 23:41:10 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '@/infra/criptography/encrypt-adapter'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository/account'
+import { SignupController } from '@/presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
+
+export const makeSignupController = (): SignupController => {
+  const emailValidator = new EmailValidatorAdapter()
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new SignupController(emailValidator, dbAccount)
+}
