a88fc4284ec5758184020ab53376b2135fbefe01
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-06 09:02:37 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,46 @@
+import { LogControllerDecorator } from './log'
+import { describe, expect, test, vi } from 'vitest'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        return await new Promise((resolve) => {
+          resolve({
+            statusCode: 200,
+            body: {
+              name: 'any_name',
+              email: 'any_email@email.com',
+              password: 'any_password',
+              passwordConfirmation: 'any_password',
+            },
+          })
+        })
+      }
+    }
+    const controllerStub = new ControllerStub()
+    const handleSpy = vi.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    })
+  })
+})
