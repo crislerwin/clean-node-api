4e7129691e2a64225aa78fbb94c0d2803723a0c7
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-08 08:30:52 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,11 +1,15 @@
 import { MissingPararmError } from '@/presentation/errors'
-import { badRequest } from '@/presentation/helpers/http-helper'
+import { badRequest, ok } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await new Promise((resolve) => {
-      resolve(badRequest(new MissingPararmError('email')))
-    })
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingPararmError('email'))
+    }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingPararmError('password'))
+    }
+    return ok({})
   }
 }
