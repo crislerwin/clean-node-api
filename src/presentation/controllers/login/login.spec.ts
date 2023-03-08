8dce7c9f1dcf21a82b5aa147d5caa7d0b35826d3
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-08 08:41:50 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,10 +1,32 @@
 import { MissingPararmError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http-helper'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+import { EmailValidator } from '../signup/signup-protocols'
 import { LoginController } from './login'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+interface SutTypes {
+  sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new LoginController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub,
+  }
+}
 describe('Login Controller', () => {
   test('should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password',
@@ -13,14 +35,16 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingPararmError('email')))
   })
-  test('should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+
+  test('Should call Emailvalidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = vi.spyOn(emailValidatorStub, 'isValid')
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
       },
     }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingPararmError('password')))
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
