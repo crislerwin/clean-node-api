5389b53de04d711d31dc4eea0a166e48eb84aa32
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-08 08:41:34 -0300

    feat: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,9 +1,15 @@
 import { MissingPararmError } from '@/presentation/errors'
 import { badRequest, ok } from '@/presentation/helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../signup/signup-protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.emailValidator.isValid(httpRequest.body.email)
     if (!httpRequest.body.email) {
       return badRequest(new MissingPararmError('email'))
     }
