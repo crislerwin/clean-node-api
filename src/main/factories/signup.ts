3585a26fc84c1e1dcf2d22fd74e5b80d19e3b9fb
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-08 21:37:02 -0300

    feat: ensure SignupController calls validation with correct values

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,9 +3,16 @@ import { BcryptAdapter } from '@/infra/criptography/encrypt-adapter'
 import { AccountMongoRepository } from '@/infra/db/mongodb/account-repository/account'
 import { LogMongoRepository } from '@/infra/db/mongodb/log-repository/log'
 import { SignupController } from '@/presentation/controllers/signup/signup'
+import { Validation } from '@/presentation/helpers/validators/validation'
 import { Controller } from '@/presentation/protocols'
 import { EmailValidatorAdapter } from '@/utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
+
+class Validator implements Validation {
+  validate(input: any): Error {
+    return new Error()
+  }
+}
 
 export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
@@ -13,7 +20,8 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signupController = new SignupController(emailValidator, dbAccount)
+  const validation = new Validator()
+  const signupController = new SignupController(emailValidator, dbAccount, validation)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logMongoRepository)
 }
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -3,31 +3,30 @@ import { InvalidParamError, MissingPararmError } from '@/presentation/errors'
 import { badRequest, ok, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { EmailValidator } from '@/presentation/protocols/email-validator'
+import { Validation } from './signup-protocols'
 
 export class SignupController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
-  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
+  private readonly validation: Validation
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidator
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingPararmError(field))
-        }
+        if (!httpRequest.body[field]) return badRequest(new MissingPararmError(field))
       }
       const { name, email, password, passwordConfirmation } = httpRequest.body
-      if (password !== passwordConfirmation) {
+      if (password !== passwordConfirmation)
         return badRequest(new InvalidParamError('passwordConfirmation'))
-      }
       const isValid = this.emailValidator.isValid(email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
+      if (!isValid) return badRequest(new InvalidParamError('email'))
 
       const account = await this.addAccount.add({ email, password, name })
       return ok(account)
