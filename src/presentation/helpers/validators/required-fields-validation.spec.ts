90e6aba1d39ac92a52fc06c82345ca2ff78a133e
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-10 08:22:17 -0300

    test: ensure RequiredFieldValidation not return if validation succeds

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -2,10 +2,19 @@ import { MissingPararmError } from '@/presentation/errors'
 import { describe, expect, test } from 'vitest'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('Required fields validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingPararmError('field'))
+  })
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeUndefined()
   })
 })
