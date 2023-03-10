432ab41f21bf9004386ce0ba4544552c68db3917
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-10 08:52:08 -0300

    test: ensure ValidationComposite not return if validation succeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -39,4 +39,9 @@ describe('ValidationComposite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+  test('Should not return if validation succeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
