b1f306ae63e32b06c917b9927b81c9d5ef710d95
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-10 08:36:39 -0300

    refactor: move sut creator to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -1,18 +1,33 @@
 import { MissingPararmError } from '@/presentation/errors'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 import { Validation } from './validation'
 import { ValidateComposite } from './validation-composite'
 
+interface SutTypes {
+  sut: ValidateComposite
+  validationStub: Validation
+}
+const makeSut = (): SutTypes => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null as unknown as Error
+    }
+  }
+
+  const validationStub = new ValidationStub()
+
+  const sut = new ValidateComposite([validationStub])
+
+  return {
+    sut,
+    validationStub,
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate(input: any): Error {
-        return new MissingPararmError('field')
-      }
-    }
-
-    const validationStub = new ValidationStub()
-    const sut = new ValidateComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    vi.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingPararmError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingPararmError('field'))
   })
