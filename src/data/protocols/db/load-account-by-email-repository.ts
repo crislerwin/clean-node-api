77b6f4452af97106966149a10aaf9fd430f080a1
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-11 09:16:54 -0300

    refactor: change Authentication and LoadAccountByEmailRepository return signature

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
-import { AccountModel } from '../../usecases/add-account/db-add-account-protocols'
+import { AccountModel } from '@/domain/models/account'
 
 export interface LoadAccountByEmailRepository {
-  load: (email: string) => Promise<AccountModel>
+  load: (email: string) => Promise<AccountModel | null>
 }
