243dde5076311527488535ade8bf2fe0c30b5e19
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-11 08:52:52 -0300

    refactor: move duplicated code to helper methods

--- src/domain/usecases/authentication/db-authentication.spec.ts
+++ src/domain/usecases/authentication/db-authentication.spec.ts
@@ -3,24 +3,41 @@ import { AccountModel } from '@/domain/models/account'
 import { describe, expect, test, vi } from 'vitest'
 import { DbAuthentication } from './db-authentication'
 
-describe('DbAuthentication Usecase', () => {
-  test('Should call LoadAccountByEmailRepository with correct email', async () => {
-    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-      async load(email: string): Promise<AccountModel> {
-        const account = {
-          id: 'valid_id',
-          name: 'valid_name',
-          email: 'valid_email',
-          password: 'valid_password',
-        }
-        return await new Promise((resolve) => {
-          resolve(account)
-        })
-      }
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email',
+  password: 'valid_password',
+})
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async load(email: string): Promise<AccountModel> {
+      return await new Promise((resolve) => {
+        resolve(makeFakeAccount())
+      })
     }
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAuthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub,
+  }
+}
+
+describe('DbAuthentication Usecase', () => {
+  test('Should  call LoadAccountByEmailRepository with correct email', async () => {
+    const { loadAccountByEmailRepositoryStub, sut } = makeSut()
     const loadSpy = vi.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
     await sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
     expect(loadSpy).toBeCalledWith('any_email@mail.com')
   })
