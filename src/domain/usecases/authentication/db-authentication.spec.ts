a0d8f19a323883d288a4a9cc4501a48ed043124e
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-11 09:00:39 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/domain/usecases/authentication/db-authentication.spec.ts
+++ src/domain/usecases/authentication/db-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { LoadAccountByEmailRepository } from '@/data/protocols/load-account-by-email-repository'
 import { AccountModel } from '@/domain/models/account'
 import { describe, expect, test, vi } from 'vitest'
+import { AuthenticationModel } from '../authentication'
 import { DbAuthentication } from './db-authentication'
 
 const makeFakeAccount = (): AccountModel => ({
@@ -33,12 +34,26 @@ const makeSut = (): SutTypes => {
     loadAccountByEmailRepositoryStub,
   }
 }
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: 'any_email@mail.com',
+  password: 'any_password',
+})
 
 describe('DbAuthentication Usecase', () => {
   test('Should  call LoadAccountByEmailRepository with correct email', async () => {
     const { loadAccountByEmailRepositoryStub, sut } = makeSut()
     const loadSpy = vi.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    await sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
+    await sut.auth(makeFakeAuthentication())
     expect(loadSpy).toBeCalledWith('any_email@mail.com')
+  })
+  test('Should  throw if LoadAccountByEmailRepository throws', async () => {
+    const { loadAccountByEmailRepositoryStub, sut } = makeSut()
+    vi.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      }),
+    )
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
   })
 })
