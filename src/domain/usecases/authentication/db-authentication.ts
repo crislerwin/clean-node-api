ae5353b67237dc228d410372569ec5d26024681e
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-12 00:00:55 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/domain/usecases/authentication/db-authentication.ts
+++ src/domain/usecases/authentication/db-authentication.ts
@@ -1,22 +1,27 @@
 import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
+import { TokenGenerator } from '@/data/protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from '../authentication'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
   constructor(
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
       await this.hashComparer.compare(authentication.password, 'hashed_password')
+      await this.tokenGenerator.generate(account.id)
     }
     return null
   }
