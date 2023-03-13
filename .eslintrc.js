039014a50636fdda582ed9d8cd602200a949f018
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-13 09:00:41 -0300

    test: ensure EncryptAdapter throws if compare throws

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-misused-promises': [
       'error',
       {
--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -21,7 +21,7 @@ describe('BcryptAdapter', () => {
   })
   test('Should return a valid hash on hash success', async () => {
     const sut = makeSut()
-    vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async (): Promise<string> => {
+    vi.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
       return await new Promise((resolve) => {
         resolve('hash')
       })
@@ -31,12 +31,12 @@ describe('BcryptAdapter', () => {
   })
   test('Should throws if bcrypt throws', async () => {
     const sut = makeSut()
-    const bcryptMock = async (): Promise<string> => {
+    // @ts-expect-error
+    vi.spyOn(bcrypt, 'hash').mockReturnValueOnce(async () => {
       return await new Promise((resolve, reject) => {
         reject(new Error())
       })
-    }
-    vi.spyOn(bcrypt, 'hash').mockReturnValueOnce(bcryptMock as any)
+    })
     const promise = await sut.hash('any_value')
     await expect(promise).rejects.toThrow()
   })
@@ -53,7 +53,7 @@ describe('BcryptAdapter', () => {
   })
   test('Should return a false when compare fails', async () => {
     const sut = makeSut()
-    vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async (): Promise<boolean> => {
+    vi.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => {
       return await new Promise((resolve) => {
         resolve(false)
       })
@@ -61,5 +61,16 @@ describe('BcryptAdapter', () => {
 
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
+  })
+  test('Should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+    // @ts-expect-error
+    vi.spyOn(bcrypt, 'compare').mockReturnValueOnce(async () => {
+      return await new Promise((resolve, reject) => {
+        reject(new Error())
+      })
+    })
+    const promise = await sut.compare('any_value', 'any_hash')
+    await expect(promise).rejects.toThrow()
   })
 })
