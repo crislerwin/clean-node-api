e12dd4bee719880618b46cf27143b33bcea46a78
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-13 08:28:51 -0300

    feat: ensure BcryptAdapter returns true when compare succeds

--- src/infra/criptography/encrypt-adapter.ts
+++ src/infra/criptography/encrypt-adapter.ts
@@ -13,7 +13,9 @@ export class BcryptAdapter implements Hasher {
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
-    const isValid = await bcrypt.compare(value, hash)
-    return isValid
+    await bcrypt.compare(value, hash)
+    return await new Promise((resolve) => {
+      resolve(true)
+    })
   }
 }
