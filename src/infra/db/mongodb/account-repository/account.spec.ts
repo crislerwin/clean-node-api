7c5c0ee143b9a4dcff3762dfea41e5269b17de79
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-15 08:31:06 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success and return null if fails

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,29 +1,43 @@
-import { test, describe, expect, beforeAll, afterAll } from 'vitest'
+import { test, describe, expect, beforeAll, afterAll, beforeEach } from 'vitest'
 import { AccountMongoRepository } from './account'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
 
 describe('Account Mongo Repository', async () => {
+  let accountCollection: Collection
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create()
     const uri = mongoServer.getUri()
     await MongoHelper.connect(uri)
+    accountCollection = await MongoHelper.getCollection('accounts')
   })
+
+  beforeEach(async () => {
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on loadByEmail success', async () => {
     const sut = new AccountMongoRepository()
-    const account = await sut.add({
+    await accountCollection.insertOne({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
     })
+    const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+  test('Should return null on loadByEmail fails', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
   })
 })
