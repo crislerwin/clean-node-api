899a467bdfad1b22aa2ec0cc4f5f431715f41a91
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-14 22:05:04 -0300

    refactor: move sut to a factory method

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,21 +2,25 @@ import { describe, expect, test, vi } from 'vitest'
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values ', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = vi.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
   test('Should return a token on sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     vi.spyOn(jwt, 'sign').mockImplementationOnce(() => 'any_token')
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
   test('Should throw if sign throws ', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     vi.spyOn(jwt, 'sign').mockImplementationOnce(async () => {
       return await new Promise((_resolve, reject) => {
         reject(new Error())
