0595898872da7d4807cbecb6a1a899ad8fd1b3ca
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-16 21:26:43 -0300

    test: add login route

--- src/main/routes/login-routes.spec.ts
+++ src/main/routes/login-routes.spec.ts
@@ -4,23 +4,27 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { setupApp } from '../config/app'
 import { Express } from 'express'
 import { agent, SuperAgentTest } from 'supertest'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 let app: Express
 let server: SuperAgentTest
+let accountCollection: Collection
 
-describe('Login Routes', () => {
-  beforeAll(async () => {
-    const mongoServer = await MongoMemoryServer.create()
-    const mongoUri = mongoServer.getUri()
-    await MongoHelper.connect(mongoUri)
-    app = await setupApp()
-    server = agent(app)
-  })
+beforeAll(async () => {
+  const mongoServer = await MongoMemoryServer.create()
+  const mongoUri = mongoServer.getUri()
+  await MongoHelper.connect(mongoUri)
+  accountCollection = await MongoHelper.getCollection('accounts')
+  app = await setupApp()
+  server = agent(app)
+})
 
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
 
+describe('Post /signup', () => {
   test('Should return 200 on signup', async () => {
     await server
       .post('/api/signup')
@@ -29,6 +33,24 @@ describe('Login Routes', () => {
         email: 'any_email@email.com',
         password: 'any_password',
         passwordConfirmation: 'any_password',
+      })
+      .expect(200)
+  })
+})
+
+describe('Post /login', () => {
+  test('Should return 200 on login', async () => {
+    const password = await hash('any_password', 12)
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password,
+    })
+    await server
+      .post('/api/login')
+      .send({
+        email: 'any_email@email.com',
+        password: 'any_password',
       })
       .expect(200)
   })
