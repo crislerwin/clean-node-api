f9748837ef462b0e17c6ed98ba2d4f76f6b1a6c6
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-18 10:06:33 -0300

    chore: change node port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.VITE_MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.VITE_PORT ?? 5050,
+  port: process.env.VITE_PORT ?? 8080,
   jwtSecret: process.env.VITE_JWT_SECRET ?? 'n3t0n=4H4',
 }
