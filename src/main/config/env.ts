d9d0e93a76aea24b4690028a3225f74da2ba521f
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-19 15:18:15 -0300

    chore: local db port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,5 @@
 export default {
-  mongoUrl:
-    process.env.MONGO_URL ??
-    'mongodb+srv://crislerwintler:MOupi7WkXg1Abxxe@cluster0.mr1p445.mongodb.net/?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'n3t0n=4H4',
 }
