9d7c1d40a296829faa2ccd6366f2057f833f06eb
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-18 21:29:48 -0300

    feat: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
   },
   rules: {
     indent: 'off',
