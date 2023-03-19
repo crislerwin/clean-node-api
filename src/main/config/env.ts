fcaea3b3d13d7d62cb003d924be6b59a5469ed29
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-18 23:31:43 -0300

    fix: port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,6 @@ export default {
   mongoUrl:
     process.env.MONGO_URL ??
     'mongodb+srv://crislerwintler:MOupi7WkXg1Abxxe@cluster0.mr1p445.mongodb.net/?retryWrites=true&w=majority',
-  port: process.env.NODE_PORT ?? 5050,
+  port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'n3t0n=4H4',
 }
