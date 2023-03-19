a3b7290ce1a225392db7b0e1f14d37ec627b4022
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-18 23:03:11 -0300

    fix: env files

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,5 @@
 export default {
-  mongoUrl:
-    process.env.VITE_MONGO_URL ??
-    'mongodb://localhost:27017/clean-node-api?retryWrites=true&w=majority',
+  mongoUrl: process.env.VITE_MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.VITE_PORT ?? 5050,
   jwtSecret: process.env.VITE_JWT_SECRET ?? 'n3t0n=4H4',
 }
