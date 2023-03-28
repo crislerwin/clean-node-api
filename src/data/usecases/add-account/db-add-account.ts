7f9d2bf7b18e9221d16403bacedf0d8e5cf447c5
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-03-28 20:41:49 -0300

    feat: ensure calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,5 +1,6 @@
 import { Hasher } from '@/data/protocols/criptography/hasher'
 import { AddAccountRepository } from '@/data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccount, AddAccountModel } from '@/domain/usecases/add-account'
 
@@ -7,12 +8,11 @@ export class DbAddAccount implements AddAccount {
   constructor(
     private readonly encrypter: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
-  ) {
-    this.encrypter = encrypter
-    this.addAccountRepository = addAccountRepository
-  }
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+  ) {}
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashedPassword = await this.encrypter.hash(accountData.password)
     const account = await this.addAccountRepository.add(
       Object.assign(accountData, { password: hashedPassword }),
