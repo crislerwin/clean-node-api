ea211f713886ecbfc3e86488ef16dede106baf6e
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-04-03 08:32:51 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyuseCase throws

--- src/presentation/controllers/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import {
   Controller,
   HttpRequest,
@@ -9,18 +9,23 @@ import {
 
 export class AddSurveyController implements Controller {
   constructor(private readonly validation: Validation, private readonly addSurvey: AddSurvey) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse | null> {
-    const { question, answers } = httpRequest.body
+    try {
+      const { question, answers } = httpRequest.body
 
-    await this.addSurvey.add({
-      question,
-      answers,
-    })
+      await this.addSurvey.add({
+        question,
+        answers,
+      })
 
-    const error = this.validation.validate(httpRequest.body)
-    if (error) return badRequest(error)
-    return await new Promise((resolve) => {
-      resolve(null)
-    })
+      const error = this.validation.validate(httpRequest.body)
+      if (error) return badRequest(error)
+      return await new Promise((resolve) => {
+        resolve(null)
+      })
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
