7e95c58281cf63cabe60ba646bf90e2d92e48b4c
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-04-03 08:33:15 -0300

    test: ensure AddSurveyController returns 500 if AddSurveyuseCase throws

--- src/presentation/controllers/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/add-survey/add-survey-controller.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vi } from 'vitest'
 import { HttpRequest, Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
 import { AddSurvey } from '@/domain/usecases/add-survey'
 
 interface SutTypes {
@@ -73,5 +73,15 @@ describe('AddSurveyController', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+  test('Should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut()
+    vi.spyOn(addSurveyStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      }),
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
