d15b1547be483893ff78dc84b16d7b253b1eb70a
Crisler Wintler <crisler.wintler@splitc.com.br>
2023-04-04 09:12:06 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/db-add-survey/db-add-survey.spec.ts
+++ src/data/usecases/db-add-survey/db-add-survey.spec.ts
@@ -2,6 +2,11 @@ import { describe, test, vi, expect } from 'vitest'
 import { DbAddSurvey } from './db-add-survey'
 import { AddSurveyRepository, AddSurveyModel } from './add-survey-protocols'
 
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
 const makeFakeSurveyData = (): AddSurveyModel => ({
   question: 'any_question',
   answers: [
@@ -11,18 +16,30 @@ const makeFakeSurveyData = (): AddSurveyModel => ({
     },
   ],
 })
+
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add(data: AddSurveyModel): Promise<void> {
+      await new Promise((resolve) => {
+        resolve(null)
+      })
+    }
+  }
+  return new AddSurveyRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+  return {
+    sut,
+    addSurveyRepositoryStub,
+  }
+}
 describe('DbAddSurvey', () => {
   test('should call AddSurveyRepository with correct values', async () => {
-    class AddSurveyRepositoryStub implements AddSurveyRepository {
-      async add(data: AddSurveyModel): Promise<void> {
-        await new Promise((resolve) => {
-          resolve(null)
-        })
-      }
-    }
-    const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+    const { addSurveyRepositoryStub, sut } = makeSut()
     const addSpy = vi.spyOn(addSurveyRepositoryStub, 'add')
-    const sut = new DbAddSurvey(addSurveyRepositoryStub)
     await sut.add(makeFakeSurveyData())
     expect(addSpy).toHaveBeenCalledWith(makeFakeSurveyData())
   })
