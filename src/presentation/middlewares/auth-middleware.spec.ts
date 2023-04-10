306b9c9383118181eed9c9fe735337e448c246c9
crislerwin <crisler.wintler@splitc.com.br>
2023-04-10 09:03:08 -0300

    test: ensure AuthMiddleware returns 403 if not headers are found

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect, describe } from 'vitest'
+import { forbidden } from '../helpers/http/http-helper'
+import { AccessDeniedError } from '../errors'
+import { AuthMiddleware } from './auth-middleware'
+
+describe('Auth Middleware', () => {
+  test('Should return 403 if no x-access-token exists in headers', async () => {
+    const sut = new AuthMiddleware()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+})
