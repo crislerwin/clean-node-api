55a29502a2f6fbb4cda5e7555d9abe624851fc7a
crislerwin <crisler.wintler@splitc.com.br>
2023-04-10 09:15:27 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,12 +1,40 @@
-import { test, expect, describe } from 'vitest'
+import { test, vi, expect, describe } from 'vitest'
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '@/domain/usecases/load-account-by-token'
+import { AccountModel } from '../controllers/login/signup/signup-controller-protocols'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password',
+})
+
+class LoadAccountByTokenStub implements LoadAccountByToken {
+  async load(accessToken: string, role?: string): Promise<AccountModel> {
+    return await new Promise((resolve) => {
+      resolve(makeFakeAccount())
+    })
+  }
+}
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+  test('Should return 403 if no x-access-token exists in headers', async () => {
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const loadSpy = vi.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token',
+      },
+    })
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
