972f34b72e2582a51fd9f1568161bfe43425c662
crislerwin <crislerwintler@gmail.com>
2023-04-12 09:19:36 -0300

    test: ensure DbLoadAccountByToken throws if any dependency throws

--- src/domain/usecases/authentication/db-load-account-by-token.spec.ts
+++ src/domain/usecases/authentication/db-load-account-by-token.spec.ts
@@ -90,4 +90,25 @@ describe('DbLoadAccountByToken UseCase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toEqual(makeFakeAccount())
   })
+
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut()
+    vi.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      }),
+    )
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow()
+  })
+  test('Should throws if LoadAccountByTokenRepository throws', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    vi.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      }),
+    )
+    const promise = sut.load('any_token', 'any_role')
+    await expect(promise).rejects.toThrow()
+  })
 })
