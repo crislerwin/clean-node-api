e170234a1771e1164c5d78ef4e7a50fae752f381
crislerwin <crislerwintler@gmail.com>
2023-04-12 08:50:23 -0300

    refactor: move sut creator to a factory method

--- src/domain/usecases/authentication/db-load-account-by-token.spec.ts
+++ src/domain/usecases/authentication/db-load-account-by-token.spec.ts
@@ -1,18 +1,35 @@
 import { Decrypter } from '@/data/protocols/criptography/decrypter'
 import { describe, test, expect, vi } from 'vitest'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(value: string): Promise<string> {
+      return await new Promise((resolve) => {
+        resolve('any_value')
+      })
+    }
+  }
+  return new DecrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypter()
+  const sut = new DbLoadAccountByToken(decrypterStub)
+  return {
+    sut,
+    decrypterStub,
+  }
+}
 describe('DbLoadAccountByToken UseCase', () => {
   test('Should call Decrypter with correct values', async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt(value: string): Promise<string> {
-        return await new Promise((resolve) => {
-          resolve('any_value')
-        })
-      }
-    }
-    const decrypterStub = new DecrypterStub()
+    const { decrypterStub, sut } = makeSut()
     const decryptSpy = vi.spyOn(decrypterStub, 'decrypt')
-    const sut = new DbLoadAccountByToken(decrypterStub)
     await sut.load('any_token')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
   })
