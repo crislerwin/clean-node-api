845ef3fcd7028f37a9fe8b29bab2f3df2d90d4d5
crislerwin <crislerwintler@gmail.com>
2023-04-17 08:25:45 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/signup/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup/signup-controller-factory.ts
@@ -1,9 +1,9 @@
 import { Authentication, AuthenticationModel } from '@/domain/usecases/authentication'
 import { SignupController } from '@/presentation/controllers/login/signup/signup-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
-import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
-import { makeSignUpValidation } from './signup-validation-factory'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
+import { makeSignUpValidation } from '../signup-validation-factory'
 
 class AuthenticationRepository implements Authentication {
   async auth(_authentication: AuthenticationModel): Promise<string | null> {
--- src/main/factories/controllers/survey/add-survey/add-survey-controller.factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller.factory.ts
@@ -1,8 +1,8 @@
 import { Controller } from '@/presentation/protocols'
-import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
 import { AddSurveyController } from '@/presentation/controllers/add-survey/add-survey-controller'
-import { makeDbAddSurvey } from '../../usecases/add-survey/db-add-survey-factory'
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeDbAddSurvey } from '../../../usecases/survey/add-survey/db-add-survey-factory'
 
 export const makeAddSurveyController = (): Controller => {
   const controller = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
