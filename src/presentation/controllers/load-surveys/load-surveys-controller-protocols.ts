2545db9fafa2b37504bfb7f2bebf93c7e3b5c08a
crislerwin <crislerwintler@gmail.com>
2023-05-19 21:21:04 -0300

    feat: should call LoadSurvey with correct values

---
+++ src/presentation/controllers/load-surveys/load-surveys-controller-protocols.ts
@@ -0,0 +1,5 @@
+export * from '@/presentation/protocols'
+export * from '@/validation/validators'
+export * from '@/domain/usecases/add-survey'
+export * from '@/domain/models/survey'
+export * from '@/domain/usecases/load-surveys'
---
+++ src/presentation/controllers/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  LoadSurveys,
+} from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor(private readonly loadSurveys: LoadSurveys) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return await new Promise((resolve) => {
+      resolve({
+        statusCode: 200,
+        body: [],
+      })
+    })
+  }
+}
