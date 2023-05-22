01ca6dc9bd5691d5f38aebc1a48c883d1f46a25a
crislerwin <crislerwintler@gmail.com>
2023-05-22 09:07:31 -0300

    test: ensure DbLoadSurveys throws if LoadSurveyRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -53,4 +53,14 @@ describe('DbLoadSurveys', () => {
     const surveys = await sut.load()
     expect(surveys).toEqual(makeFakeSurveys())
   })
+  test('Should  throw if LoadSurveysRepository throws', async () => {
+    const { loadSurveysRepository, sut } = makeSut()
+    vi.spyOn(loadSurveysRepository, 'loadAll').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      }),
+    )
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
+  })
 })
