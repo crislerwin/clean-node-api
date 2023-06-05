fb5a3e738db89a334e39bf2bbf7ccad3c3b84ec2
crislerwin <crislerwintler@gmail.com>
2023-06-05 08:54:36 -0300

    fix: signup

--- src/main/factories/controllers/signup/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup/signup-controller-factory.ts
@@ -1,19 +1,12 @@
-import { Authentication, AuthenticationModel } from '@/domain/usecases/authentication'
 import { SignupController } from '@/presentation/controllers/login/signup/signup-controller'
 import { Controller } from '@/presentation/protocols'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
 import { makeSignUpValidation } from '../signup-validation-factory'
-
-class AuthenticationRepository implements Authentication {
-  async auth(_authentication: AuthenticationModel): Promise<string | null> {
-    return null
-  }
-}
+import { makeDbAuthentication } from '@/main/factories/usecases/account/authentication/db-authentication-factory'
 
 export const makeSignUpController = (): Controller => {
-  const authenticationRepository = new AuthenticationRepository()
   return makeLogControllerDecorator(
-    new SignupController(makeDbAddAccount(), makeSignUpValidation(), authenticationRepository),
+    new SignupController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication()),
   )
 }
