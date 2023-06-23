6f4c73bdb41548331be55f416c1f39bd3a997c20
crislerwin <crislerwintler@gmail.com>
2023-06-23 09:37:37 -0300

    feat: should add a survey result if its new

---
+++ src/infra/db/mongodb/survey/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,25 @@
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SaveSurveyResultModel, SurveyResultModel } from '@/domain/models/survey-result'
+import { MongoHelper } from '../../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
+
+export class SurveyMongoResultRepository implements SaveSurveyResultRepository {
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveyResults')
+    const { value } = await surveyCollection.findOneAndUpdate(
+      {
+        _id: new ObjectId(data.surveyId),
+      },
+      {
+        $set: {
+          answer: data.answer,
+          date: data.date,
+        },
+      },
+      {
+        upsert: true,
+      },
+    )
+    return MongoHelper.map<SurveyResultModel>(value)
+  }
+}
