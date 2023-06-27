e837a8658bdf8550190fe9c4c1ed9b6ea2132152
crislerwin <crislerwintler@gmail.com>
2023-06-27 08:52:02 -0300

    refactor: move survey controllers to specific survey folder

--- src/main/factories/controllers/survey/add-survey/add-survey-controller.factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller.factory.ts
@@ -1,8 +1,8 @@
 import { Controller } from '@/presentation/protocols'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
-import { AddSurveyController } from '@/presentation/controllers/add-survey/add-survey-controller'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { makeDbAddSurvey } from '@/main/factories/usecases/survey/add-survey/db-add-survey-factory'
+import { AddSurveyController } from '@/presentation/controllers/survey/add-survey/add-survey-controller'
 
 export const makeAddSurveyController = (): Controller => {
   const controller = new AddSurveyController(makeAddSurveyValidation(), makeDbAddSurvey())
