dd07e5fb4ae66486f1c0678523f37817ebe789e2
crislerwin <crislerwintler@gmail.com>
2023-06-27 21:53:31 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/domain/models/survey-result.ts
+++ src/domain/models/survey-result.ts
@@ -1,7 +1,7 @@
 export type SurveyResultModel = {
   id: string
   surveyId: string
-  accountId: string
+  accountId?: string
   answer: string
   date: Date
 }
--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
@@ -1,24 +1,35 @@
-import { forbidden, serverError } from '../add-survey/add-survey-controller-protocols'
+import { SaveSurveyResult } from '@/domain/usecases/survey-result/save-survey-result'
+import { forbidden, ok, serverError } from '../add-survey/add-survey-controller-protocols'
 import {
   Controller,
   HttpRequest,
   HttpResponse,
   InvalidParamError,
-  ok,
   LoadSurveyById,
 } from './save-survey-result-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor(private readonly loadSurveyById: LoadSurveyById) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
+  ) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
         const answers = survey.answers.map((a) => a.answer)
         if (!answers.includes(answer)) return forbidden(new InvalidParamError('answer'))
-        return ok(survey)
+        const surveyResult = await this.saveSurveyResult.save({
+          surveyId,
+          accountId,
+          date: new Date(),
+          answer,
+        })
+        return ok(surveyResult)
       } else {
         return forbidden(new InvalidParamError('surveyId'))
       }
