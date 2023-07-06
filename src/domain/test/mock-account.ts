119729707763387f8c3acbc5a6b8943c921a3c64
crislerwin <crislerwintler@gmail.com>
2023-07-05 22:12:17 -0300

    --amend

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -2,18 +2,14 @@ import { AccountModel } from '../models/account'
 import { AddAccount, AddAccountParams } from '../usecases/account/add-account'
 import { AuthenticationParams } from '../usecases/account/authentication'
 
-export const mockAccountModel = (): AccountModel => ({
-  id: 'any_id',
-  name: 'any_name',
-  email: 'any_email@mail.com',
-  password: 'hashed_password',
-})
-
 export const mockAddAccountParams = (): AddAccountParams => ({
   name: 'any_name',
   email: 'any_email@mail.com',
   password: 'hashed_password',
 })
+
+export const mockAccountModel = (): AccountModel =>
+  Object.assign({}, mockAddAccountParams(), { id: 'any_id' })
 
 export const mockAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -26,6 +22,7 @@ export const mockAddAccount = (): AddAccount => {
   }
   return new AddAccountStub()
 }
+
 export const mockAuthentication = (): AuthenticationParams => ({
   email: 'any_email@mail.com',
   password: 'any_password',
