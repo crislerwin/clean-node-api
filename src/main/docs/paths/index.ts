c666a8cbe02c7135e604d24ff36679b09e19886e
crislerwin <crislerwintler@gmail.com>
2023-07-13 22:36:23 -0300

    docs: survey swagger docs

--- src/main/docs/paths/index.ts
+++ src/main/docs/paths/index.ts
@@ -1,4 +1,11 @@
 import { loginPath } from './login-path'
 import { signUpPath } from './signup-path'
+import { surveyPath } from './survey-path'
+import { surveyResultPath } from './survey-result-path'
 
-export default { '/login': loginPath, '/signup': signUpPath }
+export default {
+  '/login': loginPath,
+  '/signup': signUpPath,
+  '/surveys': surveyPath,
+  '/surveys/{surveyId}/results': surveyResultPath,
+}
--- src/main/docs/schemas/index.ts
+++ src/main/docs/schemas/index.ts
@@ -2,10 +2,24 @@ import { accountSchema } from './account-schema'
 import { loginParamsSchema } from './login-params-schema'
 import { errorSchema } from './error-schema'
 import { signUpParamsSchema } from './signup-params-schema'
+import { saveSurveyParamsSchema } from './save-survey-params-schema'
+import { surveyResultSchema } from './survey-result-schema'
+import { addSurveyParamsSchema } from './add-survey-params-schema'
+import { surveySchema } from './survey-schema'
+import { surveysSchema } from './surveys-schema'
+import { surveyAnswerSchema } from './survey-answer-schema'
+import { surveyResultAnswerSchema } from './survey-result-answer.schema'
 
 export default {
   account: accountSchema,
   loginParams: loginParamsSchema,
-  error: errorSchema,
   signUpParams: signUpParamsSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  error: errorSchema,
+  surveys: surveysSchema,
+  survey: surveySchema,
+  surveyAnswer: surveyAnswerSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema,
+  surveyResultAnswer: surveyResultAnswerSchema,
 }
