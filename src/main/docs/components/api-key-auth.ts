8783944ccfd26e49a784df91899eaebd3f937496
crislerwin <crislerwintler@gmail.com>
2023-07-14 09:08:52 -0300

    --amend

---
+++ src/main/docs/components/api-key-auth.ts
@@ -0,0 +1,5 @@
+export const apiKeyAuth = {
+  type: 'apiKey',
+  in: 'header',
+  name: 'x-access-token',
+}
--- src/main/docs/components/index.ts
+++ src/main/docs/components/index.ts
@@ -3,5 +3,15 @@ import { forbidden } from './forbidden'
 import { serverError } from './server-error'
 import { unauthorized } from './unauthorized'
 import { notFound } from './not-found'
+import { apiKeyAuth } from './api-key-auth'
 
-export default { badRequest, forbidden, serverError, unauthorized, notFound }
+export default {
+  badRequest,
+  forbidden,
+  serverError,
+  unauthorized,
+  notFound,
+  securitySchemes: {
+    apiKeyAuth,
+  },
+}
