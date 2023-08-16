f4997f9b185d248e55ad22ca521e6bd77a02ad54
crislerwin <crislerwintler@gmail.com>
2023-08-16 08:44:58 -0300

    refactor: move sut creator to a factory method

--- tests/data/usecases/db-load-survey-result.spec.ts
+++ tests/data/usecases/db-load-survey-result.spec.ts
@@ -5,15 +5,29 @@ import { LoadSurveyResultRepository } from '@/data/protocols/db/survey/load-surv
 import { DbLoadSurveyResult } from '@/domain/usecases/load-survey-result/db-load-survey-result'
 import { mockSurveyResultModel } from '@/tests/domain/mocks/mock-survey-result'
 
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+      return await new Promise((resolve) => resolve(mockSurveyResultModel()))
+    }
+  }
+  const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return {
+    sut,
+    loadSurveyResultRepositoryStub,
+  }
+}
+
 describe('DbLoadSurveyResult UseCase', () => {
   test('Should call  LoadSurveyResultRepository ', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
-        return await new Promise((resolve) => resolve(mockSurveyResultModel()))
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
     const loadSurveyByIdSpy = vi.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadSurveyByIdSpy).toHaveBeenLastCalledWith('any_survey_id')
