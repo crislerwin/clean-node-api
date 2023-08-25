ab58e14fefdf0bd9934d557c235fe66133dba347
crislerwin <crislerwintler@gmail.com>
2023-08-25 09:16:11 -0300

    test: ensure CheckSurveyById throws if LoadSurveyResultController throws

--- tests/main/presentation/controllers/load-survey-result-controller.spec.ts
+++ tests/main/presentation/controllers/load-survey-result-controller.spec.ts
@@ -1,9 +1,10 @@
 import { CheckSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 import { LoadSurveyResultController } from '@/presentation/controllers/survey/load-survey-result/load-survey-result-controller'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http'
+import { forbidden, serverError } from '@/presentation/helpers/http'
+import { throwError } from '@/tests/domain/mocks'
 import { faker } from '@faker-js/faker'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 
 export class CheckSurveyByIdSpy implements CheckSurveyById {
   id!: string
@@ -33,7 +34,7 @@ const mockRequest = (): LoadSurveyResultController.Request => ({
   surveyId: faker.string.uuid(),
 })
 
-describe('Load survey result controller', () => {
+describe('LoadSurveyResultController', () => {
   test('Should call LoadSurveyById with corrrect value', async () => {
     const { checkSurveyByIdSpy, sut } = makeSut()
     const request = mockRequest()
@@ -46,5 +47,11 @@ describe('Load survey result controller', () => {
     checkSurveyByIdSpy.result = false
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+  test('Shold return 500 if LoadSurveyById throws', async () => {
+    const { checkSurveyByIdSpy, sut } = makeSut()
+    vi.spyOn(checkSurveyByIdSpy, 'checkById').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
