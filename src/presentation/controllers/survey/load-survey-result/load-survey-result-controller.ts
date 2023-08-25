525646cf46ecda59e63e8ea62f6f85cf74d35796
crislerwin <crislerwintler@gmail.com>
2023-08-25 09:15:59 -0300

    feat: ensure CheckSurveyById throws if LoadSurveyResultController throws

--- src/presentation/controllers/survey/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey/load-survey-result/load-survey-result-controller.ts
@@ -1,18 +1,22 @@
 import { CheckSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 import { Controller, HttpResponse } from './load-survey-controller.protocols'
-import { forbidden } from '../add-survey/add-survey-controller-protocols'
+import { forbidden, serverError } from '../add-survey/add-survey-controller-protocols'
 import { InvalidParamError } from '../save-survey-result/save-survey-result-protocols'
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: CheckSurveyById) {}
   async handle(request: LoadSurveyResultController.Request): Promise<HttpResponse> {
-    const { surveyId } = request
-    const surveyExists = await this.loadSurveyById.checkById(surveyId)
-    if (!surveyExists) {
-      return forbidden(new InvalidParamError('surveyId'))
+    try {
+      const { surveyId } = request
+      const surveyExists = await this.loadSurveyById.checkById(surveyId)
+      if (!surveyExists) {
+        return forbidden(new InvalidParamError('surveyId'))
+      }
+      // @ts-expect-error
+      return null
+    } catch (error) {
+      return serverError(error as Error)
     }
-    // @ts-expect-error
-    return null
   }
 }
 
