6a87bbd2d667883e6731b513de9d13670da5d367
crislerwin <crislerwintler@gmail.com>
2023-10-05 22:47:01 -0300

    feat: add graphql

--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -1,6 +1,19 @@
 import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { Express } from 'express'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import http from 'http'
+import { json } from 'body-parser'
+import typeDefs from '../typedefs'
+import { resolvers } from '../resolvers'
 
-export const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+export default async (app: Express): Promise<void> => {
+  const httpServer = http.createServer(app)
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+  await server.start()
+  app.use('/graphql', json(), expressMiddleware(server))
+}
