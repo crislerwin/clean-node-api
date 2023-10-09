5e8b54b3917173bd127e3b19f26820c4169f5944
crislerwin <crislerwintler@gmail.com>
2023-10-09 08:54:17 -0300

    feat: make login resolver use login controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,8 @@
+import { adaptResolver } from '@/main/adapters/apollo-server-resolver-adapter'
+import { makeLoginController } from '@/main/factories/controllers'
+
 export const resolvers = {
   Query: {
-    login() {
-      return {
-        accessToken: 'any token',
-        name: 'any name',
-      }
-    },
+    login: async (_parent: any, args: any) => await adaptResolver(makeLoginController(), args),
   },
 }
