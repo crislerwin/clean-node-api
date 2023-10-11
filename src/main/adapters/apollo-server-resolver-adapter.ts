a2b1d50521bba2d8ca008550131f15d07c77c725
crislerwin <crislerwintler@gmail.com>
2023-10-10 22:46:14 -0300

    feat: graphql errors

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,6 +1,13 @@
 import { Controller } from '@/presentation/protocols'
+import { GraphQLError } from 'graphql'
 
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
+  const errorCodes: number[] = [400, 401, 403, 500]
+  if (errorCodes.includes(httpResponse.statusCode)) {
+    throw new GraphQLError(httpResponse.body.message, {
+      extensions: { code: httpResponse.statusCode },
+    })
+  }
   return httpResponse.body
 }
--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -6,13 +6,36 @@ import http from 'http'
 import { json } from 'body-parser'
 import typeDefs from '../typedefs'
 import { resolvers } from '../resolvers'
+import { GraphQLError } from 'graphql'
+
+const errorMap: Record<string, number> = {
+  Unauthorized: 401,
+  ServerError: 500,
+  BadRequest: 400,
+  AccessDenied: 401,
+  InvalidParam: 401,
+}
+
+const handleErrors = (response: any, errors?: readonly GraphQLError[] | undefined): void => {
+  errors?.forEach((error) => {
+    response.data = undefined
+    response.http.status = errorMap?.[error.message] ?? 500
+  })
+}
 
 export default async (app: Express): Promise<void> => {
   const httpServer = http.createServer(app)
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        requestDidStart: async () => ({
+          willSendResponse: async ({ response, errors }) => handleErrors(response, errors),
+        }),
+      },
+    ],
   })
   await server.start()
   app.use('/graphql', json(), expressMiddleware(server))
